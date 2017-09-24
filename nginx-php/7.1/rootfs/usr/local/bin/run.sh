c9483a0d2eb00dd38d9248fed7057390fab85ac4
Starbix <cedric@laubacher.io>
2017-09-24 16:18:05 +0200

    maybe fix user

--- nginx-php/7.1/rootfs/usr/local/bin/run.sh
+++ nginx-php/7.1/rootfs/usr/local/bin/run.sh
@@ -1,5 +1,14 @@
-#/bin/sh
+#!/bin/sh
 
-sync
+echo "Updating permissions..."
+for dir in /var/log /php /nginx /tmp /etc/s6.d; do
+  if $(find $dir ! -user $UID -o ! -group $GID|egrep '.' -q); then
+    echo "Updating permissions in $dir..."
+    chown -R $UID:$GID $dir
+  else
+    echo "Permissions in $dir are correct."
+  fi
+done
+echo "Done updating permissions."
 
-exec su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
+su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
